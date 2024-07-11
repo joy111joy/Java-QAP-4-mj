7db638db868891096952dc6306a9917bb665e9e4
joy111joy <mitchel_joy@hotmail.com>
2024-07-11 10:35:52 -0230

    Finished Problem 4

--- Problem4/ParseInts.java
+++ Problem4/ParseInts.java
@@ -13,24 +13,27 @@ int val, sum=0;
 
 Scanner scan = new Scanner(System.in);
 
-String line;
+// Remove the unused variable declaration
 
 System.out.println("Enter a line of text");
 
 Scanner scanLine = new Scanner(scan.nextLine());
-
 while (scanLine.hasNext())
 
 {
-
+try {
 val = Integer.parseInt(scanLine.next());
-
 sum += val;
+
+} catch (NumberFormatException e) {}
+
+
 
 }
 
 System.out.println("The sum of the integers on this line is " + sum);
 
+scan.close();
+scanLine.close();
 }
-
 }
